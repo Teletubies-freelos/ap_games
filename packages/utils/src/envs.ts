bbfb2647b0cd1ec758c6c9637ce7285a2e5b5d42
luiggy <luiggylml@gmail.com>
2023-10-15 22:17:54 -0500

    fix: comment ts ignore on package utils

--- packages/utils/src/envs.ts
+++ packages/utils/src/envs.ts
@@ -1,39 +1,36 @@
-export interface GetEnvOptions{
+export interface GetEnvOptions {
   prefix?: string;
   isRequired?: boolean;
 }
 
-export class RequiredEnvError extends Error{
-  constructor(envName: string){
+export class RequiredEnvError extends Error {
+  constructor(envName: string) {
     super(`
       Env variable ${envName} is required to start project,
       check your .env file
-    `)
+    `);
   }
 }
 
-export class Config{
-  constructor(private defaultOptions: GetEnvOptions){}
+export class Config {
+  constructor(private defaultOptions: GetEnvOptions) {}
 
-  private mergeOptions(options: GetEnvOptions){
+  private mergeOptions(options: GetEnvOptions) {
     return {
       ...this.defaultOptions,
-      ...options
-    }
+      ...options,
+    };
   }
 
+  getEnv(envName: string, options: GetEnvOptions = {}) {
+    const { isRequired, prefix = '' } = this.mergeOptions(options);
+    const envCompleteName = `${prefix}${envName}`;
 
-  getEnv(envName: string, options: GetEnvOptions = {}){
-    const { isRequired, prefix = '' } = this.mergeOptions(options)
-    const envCompleteName = `${prefix}${envName}`
+    //@ts-ignore
+    const value = import.meta.env[envCompleteName];
 
-    //@ts-expect-error
-    const value = import.meta.env[envCompleteName]
-    
-    if(isRequired && !value)
-      throw new RequiredEnvError(envCompleteName)
+    if (isRequired && !value) throw new RequiredEnvError(envCompleteName);
 
-    return value
+    return value;
   }
 }
-
