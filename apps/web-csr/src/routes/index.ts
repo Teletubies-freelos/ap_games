c7bb5878ff80fbb91e1b8ede590fe0bab1bb4beb
luiggy <luiggylml@gmail.com>
2023-10-15 22:37:00 -0500

    chore: prepare build viteconfig

--- apps/web-csr/src/routes/index.ts
+++ apps/web-csr/src/routes/index.ts
@@ -1,14 +1,12 @@
-import { createBrowserRouter, createHashRouter } from "react-router-dom";
-import { isHashProvider } from "../config/router";
-import { lazy } from "react";
+import { createBrowserRouter, createHashRouter } from 'react-router-dom';
+import { isHashProvider } from '../config/router';
+import { lazy } from 'react';
 
-const routerFn = isHashProvider ? createHashRouter : createBrowserRouter
-
-
+const routerFn = isHashProvider ? createHashRouter : createBrowserRouter;
 
 export const routes = routerFn([
   {
     path: '/',
-    Component: lazy(()=>import('../pages/Home'))
-  }
-])
+    Component: lazy(() => import('../pages/Home')),
+  },
+]);
--- apps/web-csr/vite.config.ts
+++ apps/web-csr/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './',
+  build: {
+    rollupOptions: {
+      manualChunks: {
+        react: ['react', 'react-dom'],
+        material: ['@mui/material'],
+        emotion: ['@emotion/styled', '@emotion/react'],
+        router: ['react-router-dom'],
+        reactQuery: ['@tanstack/react-query'],
+      },
+    },
+  },
+});
