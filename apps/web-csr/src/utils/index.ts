390df996ff8e972e61db3ad71fe0bed0b257f037
luiggy <luiggylml@gmail.com>
2023-10-17 18:35:34 -0500

    chore: flow modals

--- apps/web-csr/src/utils/index.ts
+++ apps/web-csr/src/utils/index.ts
@@ -1,49 +1,53 @@
-import { useEffect, useState } from "react";
-import type { BehaviorSubject } from "rxjs";
-import { ICartProduct } from "../data/indexedDB"
+import { useEffect, useState } from 'react';
+import type { BehaviorSubject } from 'rxjs';
+import { ICartProduct } from '../data/indexedDB';
 
-export const reduceTotalPrice = (products?: ICartProduct[]) => products
-  ?.reduce(
-    (acm, {quantity, price, priceDiscount})=> acm  + (priceDiscount || price)*quantity,
+export const reduceTotalPrice = (products?: ICartProduct[]) =>
+  products?.reduce(
+    (acm, { quantity, price, priceDiscount }) =>
+      acm + (priceDiscount || price) * quantity,
     0
-  )
+  );
 
-export const reduceQuantity = (products?: ICartProduct[]) => products
-    ?.reduce(
-      (acm, {quantity})=> acm + quantity, 
-      0
-    )
+export const reduceQuantity = (products?: ICartProduct[]) =>
+  products?.reduce((acm, { quantity }) => acm + quantity, 0);
 
-export interface ControlState<Data>{
+export interface ControlState<Data> {
   data?: Data;
-  previousState?: ControlState<Data>
+  previousState?: ControlState<Data>;
 }
 
-export function bindLense<T = unknown>(subject: BehaviorSubject<T>):[(nextValue: T )=> void, () => T]{
+export function bindLense<T = unknown>(
+  subject: BehaviorSubject<T>
+): [(nextValue: T) => void, () => T] {
   return [
-    value => subject.next(value),
-    ()=>{
-      const [ value, setValue ] = useState<T>(subject.value)
+    (value) => subject.next(value),
+    () => {
+      const [value, setValue] = useState<T>(subject.value);
 
-      useEffect(()=>{
+      useEffect(() => {
         const subscription = subject.subscribe(setValue);
 
-        return ()=> subscription.unsubscribe();
-      }, [])
+        return () => subscription.unsubscribe();
+      }, []);
 
-      return value
-    }
-  ]
+      return value;
+    },
+  ];
 }
 
-export function orderControlsLense<T>(subject: BehaviorSubject<ControlState<T> | undefined>){
-  const [setState, useObserverState] = bindLense(subject)
+export function orderControlsLense<T>(
+  subject: BehaviorSubject<ControlState<T> | undefined>
+) {
+  const [setState, useObserverState] = bindLense(subject);
   return {
     setState,
     useObserverState,
     setPrevState: () => setState(subject.value?.previousState),
-    setNextState: (nextStateName: T) => setState({
-    data: nextStateName,
-    previousState: subject.value
-  })}
+    setNextState: (nextStateData: T) =>
+      setState({
+        data: nextStateData,
+        previousState: subject.value,
+      }),
+  };
 }
