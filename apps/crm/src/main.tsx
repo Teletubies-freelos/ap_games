e9baa8cd9251adbad9d19ca322ed265d12bed1c9
Shadow <pieroramirez810@gmail.com>
2023-10-22 18:32:53 -0500

    feat: woopra

--- apps/crm/src/main.tsx
+++ apps/crm/src/main.tsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-// import './index.css'
-import { env } from './config/index.ts'
+import App from './App.js'
 
-if(import.meta.env.DEV && env.IS_MSW_ON){
-  const { worker } = await import('../../../packages/mock-service/src/browser')
-
-  worker.start()
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
