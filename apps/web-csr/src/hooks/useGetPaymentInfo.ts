464dbbedce6e74fb52ed1ecb872a60fb8a71c63b
Piero Carrión <piero.january15@gmail.com>
2023-11-22 00:05:01 -0500

    fixes

--- apps/web-csr/src/hooks/useGetPaymentInfo.ts
+++ apps/web-csr/src/hooks/useGetPaymentInfo.ts
@@ -1,20 +1,18 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useGetList, useSyncGetOne } from "data_providers";
 import { UserInfo } from "../services/SessionStorage";
 import { ProviderNames } from "../types/providers";
 import { IDataPayment } from "../components/ConfirmOrder/ConfirmOrderDelivery";
 
 export const useGetPaymentInfo = () => {
-  const queryClient = useQueryClient();
   const getClientData = useSyncGetOne<UserInfo>(ProviderNames.SESSION_STORAGE);
   const { payment_method_id } = getClientData()
 
-  const getPaymentMethods = useGetList<IDataPayment[]>(ProviderNames.PAYMENT_METHODS);
+  const getPaymentMethods = useGetList<IDataPayment>(ProviderNames.PAYMENT_METHODS);
   const { data } = useQuery(
     ['payment_methods'],
     async () => await getPaymentMethods()
   );
-    // @ts-ignore
   const infoPayment = data?.filter((item: IDataPayment) => item.payment_method_id == Number(payment_method_id))
 
   return infoPayment ?? [];
